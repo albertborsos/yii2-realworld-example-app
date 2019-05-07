36345b4c4d5196e9cea69db8ca5bc64bcf809faa
Albert Borsos <albertborsos@me.com>
2019-05-07 10:31:00 +0200

    composer install

--- config/web.php
+++ config/web.php
@@ -13,8 +13,7 @@ $config = [
     ],
     'components' => [
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'rXVaWYxMOJyzDm8xNAyotFsGxZDT5WEk',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
