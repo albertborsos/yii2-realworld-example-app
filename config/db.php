b06de9bab306c46b66b2a558624401cdd13dbc04
Albert Borsos <albertborsos@me.com>
2019-05-07 10:14:52 +0200

    configure database for docker

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=db;dbname=database',
+    'username' => 'user',
+    'password' => 'pass',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
